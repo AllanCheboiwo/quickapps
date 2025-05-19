7ee78fc8c3afefe0fb19fff2b0ba2ef53cd0c0c6
AllanCheboiwo <kiplongeiallan@gmail.com>
2025-05-19 12:24:21 -0700

    Database setup and models update

---
+++ backend/userService/src/utils/db.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine,text
+from sqlalchemy.orm import DeclarativeBase,sessionmaker
+from typing import Generator
+import os
+from dotenv import load_dotenv
+
+class Base(DeclarativeBase):
+    pass
+
+
+#load environment variables
+load_dotenv()
+
+#Database URL configurations
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+#database url
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+#create sqlalchemy engine
+engine = create_engine(DATABASE_URL,echo = True)
+
+#create ORM session for engine
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#create base class
+Base = declarative_base()
+
+#Dependency to get DB session
+
+def get_db() -> Generator:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
